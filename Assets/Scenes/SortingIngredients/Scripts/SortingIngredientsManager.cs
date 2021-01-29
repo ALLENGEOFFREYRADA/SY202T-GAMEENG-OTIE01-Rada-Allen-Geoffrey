71710edf57c9031f82d885c891bbd99e9a9f0ca3
alllenski <allengeoffrey.rada@benilde.edu.ph>
2021-01-30 01:13:19 +0800

    Added sorting display

--- Assets/Scenes/SortingIngredients/Scripts/SortingIngredientsManager.cs
+++ Assets/Scenes/SortingIngredients/Scripts/SortingIngredientsManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SortingIngredientsManager : MonoBehaviour
 {
@@ -10,12 +11,16 @@ public class SortingIngredientsManager : MonoBehaviour
     public Sprite[] sprites;
     public GameObject leftSorter;
     public GameObject rightSorter;
+    public GameObject leftDisplay;
+    public GameObject rightDisplay;
     private List<GameObject> ingredients = new List<GameObject>();
 
     void Start()
     {
         int leftIngredientIndex = Random.Range(0, sprites.Length);
         int rightIngredientIndex = Random.Range(0, sprites.Length);
+        SpriteRenderer spriteRenderer;
+
         if (rightIngredientIndex == leftIngredientIndex) 
         {
             rightIngredientIndex++;
@@ -29,22 +34,27 @@ public class SortingIngredientsManager : MonoBehaviour
         for (int i = 0; i < ingredientsCount / 2; i++)
         {
             int x = Random.Range(-10, 11);
-            int y = Random.Range(-4, -1);
+            int y = Random.Range(1, 4);
             ingredients.Add(Instantiate(ingredientPrefab, new Vector3(x, y, 0), Quaternion.identity) as GameObject);
             ingredients[ingredients.Count - 1].tag = "leftIngredient";
-            SpriteRenderer spriteRenderer = ingredients[ingredients.Count - 1].GetComponent<Renderer>() as SpriteRenderer;
+            spriteRenderer = ingredients[ingredients.Count - 1].GetComponent<Renderer>() as SpriteRenderer;
             spriteRenderer.sprite = sprites[leftIngredientIndex];
         }
 
         for (int i = 0; i < ingredientsCount / 2; i++)
         {
             int x = Random.Range(-10, 11);
-            int y = Random.Range(-4, -1);
+            int y = Random.Range(1, 4);
             ingredients.Add(Instantiate(ingredientPrefab, new Vector3(x, y, 0), Quaternion.identity) as GameObject);
             ingredients[ingredients.Count - 1].tag = "rightIngredient";
-            SpriteRenderer spriteRenderer = ingredients[ingredients.Count - 1].GetComponent<Renderer>() as SpriteRenderer;
+            spriteRenderer = ingredients[ingredients.Count - 1].GetComponent<Renderer>() as SpriteRenderer;
             spriteRenderer.sprite = sprites[rightIngredientIndex];
         }
+
+        spriteRenderer = leftDisplay.GetComponent<Renderer>() as SpriteRenderer;
+        spriteRenderer.sprite = sprites[leftIngredientIndex];
+        spriteRenderer = rightDisplay.GetComponent<Renderer>() as SpriteRenderer;
+        spriteRenderer.sprite = sprites[rightIngredientIndex];
 
     }
 
@@ -52,7 +62,7 @@ public class SortingIngredientsManager : MonoBehaviour
     {
         if (leftSorter.GetComponent<CheckCollisions>().collisions == 2 && rightSorter.GetComponent<CheckCollisions>().collisions == 2)
         {
-            Debug.Log("win!");
+            SceneManager.LoadScene(sceneName:"SortingIngredients");
         }
     }
     
